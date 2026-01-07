6fcce887a33a70bb90c1d849e0c8dc5162cbbf52
Jakub Kaczor <j.kaczor06@gmail.com>
2026-01-07 15:20:32 +0100

    added viewport boundaries

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,42 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float leftBoundPadding = 5f;
+    [SerializeField] float rightBoundPadding = 5f;
+    [SerializeField] float topBoundPadding = 5f;
+    [SerializeField] float bottomBoundPadding = 5f;
     InputAction moveAction;
     Vector3 moveVector;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+
+        InitBounds();
     }
 
-    // Update is called once per frame
     void Update()
     {
         MovePlayer();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0f,0f));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1f,1f));
+    }
+
     void MovePlayer()
     {
         moveVector = moveAction.ReadValue<Vector2>();
-        transform.position += moveVector * Time.deltaTime * moveSpeed;
+        Vector3 newPos = transform.position + moveVector * moveSpeed * Time.deltaTime;
+
+        newPos.x = Mathf.Clamp(newPos.x, minBounds.x + leftBoundPadding, maxBounds.x - rightBoundPadding);
+        newPos.y = Mathf.Clamp(newPos.y, minBounds.y + bottomBoundPadding, maxBounds.y - topBoundPadding);
+
+        transform.position = newPos;
     }
 }
